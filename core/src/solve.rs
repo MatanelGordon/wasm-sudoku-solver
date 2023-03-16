417d5433afb2ca101e3def07318aefb8ac715f95
Matanel Mac <mmgordon.matanel@gmail.com>
2023-03-16 16:02:09 +0200

    removed mock function

--- core/src/solve.rs
+++ core/src/solve.rs
@@ -1,8 +1,4 @@
+use crate::analyze::analyze_board;
 use crate::board::Board;
 use crate::types::StrResult;
 use std::vec;
-
-pub fn solve(board: &Board) -> StrResult<Vec<Board>> {
-    let some_board = Board::<usize>::new(4)?;
-    return Ok(vec![some_board]);
-}
