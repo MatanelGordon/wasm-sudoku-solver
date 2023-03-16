12752ad2e18320bebf238c4cefcb2dae4f6c6a77
Matanel Mac <mmgordon.matanel@gmail.com>
2023-03-16 22:34:54 +0200

    Removed unused imports

--- core/src/solve.rs
+++ core/src/solve.rs
@@ -1,6 +1,5 @@
-use crate::analyze::{analyze_board, AnalyzedBoard};
+use crate::analyze::AnalyzedBoard;
 use crate::board::Board;
-use crate::types::StrResult;
 use std::vec;
 
 fn solve_inner(board: &AnalyzedBoard) -> Vec<AnalyzedBoard> {
