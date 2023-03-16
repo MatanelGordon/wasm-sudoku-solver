8e742f756b4efb80dca75bb6bf4de40f2058b783
Matanel Mac <mmgordon.matanel@gmail.com>
2023-03-16 16:02:31 +0200

    Added tests and new mod analyze_tests

--- core/src/tests.rs
+++ core/src/tests.rs
@@ -1,5 +1,5 @@
 #[cfg(test)]
-pub mod tests {
+pub mod board_tests {
     use crate::board::{Board, BoardData};
 
     #[test]
@@ -41,11 +41,73 @@ pub mod tests {
         assert_eq!(*received, expected);
     }
 
-    // #[test]
-    // fn analyze_cell_test(){
-    //     let data: BoardData = vec![vec![1,2,3,0],vec![3,0,0,0],vec![0,0,0,0],vec![0,0,0,0]];
-    //     let board = Board::from(&data).expect("Failed creating board");
-    //
-    //     let cell = analyze_cell(&board, 1,1);
-    // }
+    #[test]
+    fn flat_row_correct_length(){
+        let size: usize = 4;
+        let board = Board::new(size).expect("could not create board");
+        assert_eq!(board.get_rows_flat().len(), size.pow(2));
+    }
+
+    #[test]
+    fn board_is_full(){
+        let data: BoardData<usize> =  vec![
+            vec![1,1,1,1],
+            vec![1,1,1,1],
+            vec![1,1,1,1],
+            vec![1,1,1,1]
+        ];
+        let board: Board = Board::from(&data).expect("Failed creating board");
+        assert!(board.is_full())
+    }
+
+    #[test]
+    fn board_is_not_full(){
+        let data: BoardData<usize> =  vec![
+            vec![1,1,1,1],
+            vec![1,1,0,1],
+            vec![1,0,1,1],
+            vec![1,1,1,1]
+        ];
+        let board: Board = Board::from(&data).expect("Failed creating board");
+        assert!(!board.is_full())
+    }
+
+    #[test]
+    fn board_invalid_numerical(){
+        let mut board = Board::new(4).expect("Failed initializing board");
+        board.set(2,2,100);
+        assert_eq!(board.is_valid_numerical(), false);
+    }
+}
+
+#[cfg(test)]
+pub mod analyze_tests {
+    use crate::analyze::{analyze_board, analyze_cell, AnalyzedCell, is_invalid_analyze};
+    use crate::board::Board;
+
+    #[test]
+    fn determine_value() {
+        let mut board = Board::new(9).expect("Failed setting up board");
+        let row: usize = 2;
+        let col: usize = 2;
+        let value: usize = 4;
+        board.set(row, col, value);
+        let analyzed = analyze_cell(&board, row, col).expect("Failed analyzing cell");
+        assert_eq!(analyzed, AnalyzedCell::Value(value));
+    }
+
+    #[test]
+    fn invalidate_board(){
+        //creating invalid board
+        let mut board = Board::new(4).expect("Failed setting up board");
+        board.set(0,0,1);
+        board.set(0,1,2);
+        board.set(1,0,3);
+        board.set(1,3,4);
+
+        let analyzed = analyze_board(&board).expect("could not analyze board");
+        let is_invalid = is_invalid_analyze(&analyzed);
+
+        assert!(is_invalid);
+    }
 }
