f8d02ea830c66e68436b5ef63ada7fb581c09b56
Matanel Mac <mmgordon.matanel@gmail.com>
2023-03-16 21:09:02 +0200

    moved code around

--- core/src/analyze.rs
+++ core/src/analyze.rs
@@ -1,6 +1,5 @@
 use crate::board::{Board, BoardData};
 use std::collections::HashSet;
-use std::iter::Enumerate;
 
 #[derive(Debug, Clone, PartialEq)]
 pub enum AnalyzedCell {
@@ -10,6 +9,73 @@ pub enum AnalyzedCell {
 
 pub type AnalyzedBoardData = BoardData<AnalyzedCell>;
 pub type AnalyzedBoard = Board<AnalyzedCell>;
+
+struct InferredPosition {
+    pub row: usize,
+    pub col: usize,
+    pub value: usize,
+}
+
+fn infer_square_reduction(analyzed_board: &AnalyzedBoard) -> Vec<InferredPosition> {
+    let size = analyzed_board.get_size();
+    let square_size = analyzed_board.get_square_size();
+    let mut square_inferring: Vec<InferredPosition> = vec![];
+    //narrowing down the options using smart inferring
+    for square_row in 0..analyzed_board.get_square_size() {
+        for square_col in 0..analyzed_board.get_square_size() {
+            let square = analyzed_board.get_square(square_row, square_col).unwrap();
+
+            let flatten_options = square.iter().flat_map(|a| match a {
+                AnalyzedCell::Value(val) => vec![*val],
+                AnalyzedCell::Undetermined(options) => options.to_vec(),
+            });
+
+            let single_repeating_values = (1..=size)
+                .into_iter()
+                .filter(|&n| flatten_options.clone().filter(|&val| val == n).count() == 1);
+
+            let enumerated_square_options = square
+                .iter()
+                .enumerate()
+                .filter(|&(i, analyzed)| matches!(analyzed, AnalyzedCell::Undetermined(_)));
+
+            single_repeating_values
+                .clone()
+                .map(|value| {
+                    // find the square index which value belongs to.
+                    let chosen_index = enumerated_square_options
+                        .clone()
+                        .filter(|&(index, analyzed)| {
+                            if let AnalyzedCell::Undetermined(options) = analyzed {
+                                return options.iter().find(|&&opt| opt == value).is_some();
+                            }
+                            false
+                        })
+                        .map(|x| x.0)
+                        .next()
+                        .unwrap();
+                    return (chosen_index, value);
+                })
+                .for_each(|(index, value)| {
+                    let inner_row = index / square_size;
+                    let inner_col = index % square_size;
+                    let row = square_row * square_size + inner_row;
+                    let col = square_col * square_size + inner_col;
+
+                    square_inferring.push(InferredPosition { value, row, col })
+                });
+        }
+    }
+    return square_inferring;
+}
+
+pub fn is_full_board(board: &AnalyzedBoard) -> bool {
+    board
+        .get_rows_flat()
+        .into_iter()
+        .find(|&cell| matches!(cell, &AnalyzedCell::Undetermined(_)))
+        .is_none()
+}
 
 pub fn analyze_cell(board: &Board, row: usize, col: usize) -> Option<AnalyzedCell> {
     let value_ref = board.at(row, col)?;
@@ -54,71 +120,6 @@ pub fn analyze_cell(board: &Board, row: usize, col: usize) -> Option<AnalyzedCel
     }
 
     Some(AnalyzedCell::Undetermined(possible_options))
-}
-
-struct InferredPosition {
-    pub row: usize,
-    pub col: usize,
-    pub value: usize,
-}
-
-fn infer_square_reduction(analyzed_board: &AnalyzedBoard) -> Vec<InferredPosition> {
-    let size = analyzed_board.get_size();
-    let square_size = analyzed_board.get_square_size();
-    let mut square_inferring: Vec<InferredPosition> = vec![];
-    //narrowing down the options using smart inferring
-    for square_row in 0..analyzed_board.get_square_size() {
-        for square_col in 0..analyzed_board.get_square_size() {
-            let square = analyzed_board.get_square(square_row, square_col).unwrap();
-
-            let flatten_options: Vec<usize> = square
-                .iter()
-                .flat_map(|a| match a {
-                    AnalyzedCell::Value(val) => vec![*val],
-                    AnalyzedCell::Undetermined(options) => options.to_vec(),
-                })
-                .collect();
-
-            let single_repeating_values: Vec<usize> = (1..=size)
-                .into_iter()
-                .filter(|&n| flatten_options.iter().filter(|&&val| val == n).count() == 1)
-                .collect();
-
-            let enumerated_square_options: Vec<(usize, &AnalyzedCell)> =
-                square
-                    .iter()
-                    .enumerate()
-                    .filter(|&(i, analyzed)| matches!(analyzed, AnalyzedCell::Undetermined(_)))
-                    .collect();
-
-            single_repeating_values
-                .iter()
-                .map(|&value| {
-                    // find the square index which value belongs to.
-                    let chosen_index = enumerated_square_options
-                        .iter()
-                        .filter(|&(index, analyzed)| {
-                            if let AnalyzedCell::Undetermined(options) = analyzed {
-                                return options.iter().find(|&&opt| opt == value).is_some()
-                            }
-                            false
-                        })
-                        .map(|&x| x.0)
-                        .next()
-                        .unwrap();
-                    return (chosen_index, value);
-                })
-                .for_each(|(index, value)| {
-                    let inner_row = index / square_size;
-                    let inner_col = index % square_size;
-                    let row = square_row * square_size + inner_row;
-                    let col = square_col * square_size + inner_col;
-
-                    square_inferring.push(InferredPosition { value, row, col })
-                });
-        }
-    }
-    return square_inferring;
 }
 
 pub fn analyze_board(board: &Board) -> Option<AnalyzedBoard> {
