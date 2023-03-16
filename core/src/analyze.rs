cf9f2fcd7192b76ad5b9c0793958ea3df8f59793
Matanel Mac <mmgordon.matanel@gmail.com>
2023-03-16 16:01:41 +0200

    rename + impl is_invalid_analyze

--- core/src/analyze.rs
+++ core/src/analyze.rs
@@ -1,28 +1,29 @@
-use crate::board::Board;
+use crate::board::{Board, BoardData};
 use std::collections::HashSet;
 
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, PartialEq)]
 pub enum AnalyzedCell {
     Invalid,
-    Determined(usize),
+    Value(usize),
     Undetermined(Vec<usize>),
-    Default,
 }
 
-pub type AnalyzedBoard = Vec<Vec<AnalyzedCell>>;
+pub type AnalyzedBoardData = BoardData<AnalyzedCell>;
+pub type AnalyzedBoard = Board<AnalyzedCell>;
 
 pub fn analyze_cell(board: &Board, row: usize, col: usize) -> Option<AnalyzedCell> {
     let value_ref = board.at(row, col)?;
     let value = *value_ref;
 
     if value > 0 {
-        return Some(AnalyzedCell::Determined(value));
+        return Some(AnalyzedCell::Value(value));
     }
 
     let size = board.get_size();
     let row_list = board.get_row(row)?;
     let col_list = board.get_col(col)?;
     let square_list = board.get_square(row, col)?;
+
     let mut all_axis_options: Vec<usize> = Vec::new();
     let all_options: HashSet<usize> = (1..=size).collect::<HashSet<_>>();
 
@@ -45,10 +46,28 @@ pub fn analyze_cell(board: &Board, row: usize, col: usize) -> Option<AnalyzedCel
     }
 
     if possible_options.len() == 1 {
-        return Some(AnalyzedCell::Determined(possible_options[0]));
+        return Some(AnalyzedCell::Value(possible_options[0]));
     }
 
     Some(AnalyzedCell::Undetermined(possible_options))
 }
 
-pub fn analyzeBoard(board: &Board) {}
+pub fn is_invalid_analyze(analyzed_board: &AnalyzedBoard) -> bool {
+    analyzed_board.get_rows_flat().iter().find(|val| ***val == AnalyzedCell::Invalid ).is_some()
+}
+
+pub fn analyze_board(board: &Board) -> Option<AnalyzedBoard> {
+    let size = board.get_size();
+    let mut board_size_data: AnalyzedBoardData = vec![];
+
+    for row in 0..size {
+        let mut row_list: Vec<AnalyzedCell> = vec![];
+        for col in 0..size {
+            let cell = analyze_cell(board, row, col)?;
+            row_list.push(cell);
+        }
+        board_size_data.push(row_list);
+    }
+
+    return Board::from(&board_size_data).ok();
+}
