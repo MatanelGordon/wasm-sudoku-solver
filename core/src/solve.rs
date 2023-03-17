9bc9a87ecb0fd4e15e5287966c3406f5908d3616
Matanel Mac <mmgordon.matanel@gmail.com>
2023-03-17 18:01:25 +0200

    Added solve boilerplate

--- core/src/solve.rs
+++ core/src/solve.rs
@@ -1,11 +1,21 @@
-use crate::analyze::AnalyzedBoard;
-use crate::board::Board;
 use std::vec;
+use rand::rngs::ThreadRng;
+use rand::thread_rng;
+use crate::types::StrResult;
+use crate::analyze::{analyze_board, AnalyzedBoard};
+use crate::board::Board;
 
-fn solve_inner(board: &AnalyzedBoard) -> Vec<AnalyzedBoard> {
-    vec![]
+fn inner_solve(analyzed_board: &AnalyzedBoard, rand_thread: &mut ThreadRng) -> StrResult<AnalyzedBoard>{
+    todo!()
 }
 
-pub fn solve(board: &Board) -> Vec<Board> {
-    vec![]
+fn to_board(analyzed_board: &AnalyzedBoard) -> StrResult<Board> {
+    todo!()
+}
+
+pub fn solve (board: &Board) -> StrResult<Board>{
+    let analyzed_board = analyze_board(board).ok_or(format!("Could not analyze board"))?;
+    let mut rand_thread = rand::thread_rng();
+    let solved = inner_solve(&analyzed_board, &mut rand_thread)?;
+    to_board(&solved)
 }
