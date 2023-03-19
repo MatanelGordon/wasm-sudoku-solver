50bcfb0dd6690abb15c76636e44f8dd2cd8e26f4
Matanel Mac <mmgordon.matanel@gmail.com>
2023-03-19 17:55:33 +0200

    removed imports

--- core/src/solve.rs
+++ core/src/solve.rs
@@ -1,20 +1,13 @@
-use crate::analyze::{analyze_board, AnalyzedBoard, AnalyzedCell, to_board};
-use crate::board::{Board, BoardData};
+use crate::analyze::{analyze_board, to_board, AnalyzedBoard};
+use crate::board::Board;
 use crate::types::StrResult;
-use rand::rngs::ThreadRng;
-use rand::thread_rng;
-use std::vec;
 
-fn inner_solve(
-    analyzed_board: &AnalyzedBoard,
-    rand_thread: &mut ThreadRng,
-) -> StrResult<AnalyzedBoard> {
+fn inner_solve(analyzed_board: &AnalyzedBoard) -> StrResult<AnalyzedBoard> {
     todo!()
 }
 
 pub fn solve(board: &Board) -> StrResult<Board> {
     let analyzed_board = analyze_board(board).ok_or(format!("Could not analyze board"))?;
-    let mut rand_thread = rand::thread_rng();
-    let solved = inner_solve(&analyzed_board, &mut rand_thread)?;
+    let solved = inner_solve(&analyzed_board)?;
     to_board(&solved)
 }
