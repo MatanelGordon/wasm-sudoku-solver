3318ed2b1d21c8fc22bc010a59baa991b813c337
Matanel Mac <mmgordon.matanel@gmail.com>
2023-04-01 22:15:20 +0300

    Added support to create sudokus

--- core/src/solve.rs
+++ core/src/solve.rs
@@ -2,30 +2,34 @@ use crate::analyze::{analyze_board, is_full_board, to_board, AnalyzedBoard, Anal
 use crate::board::Board;
 use crate::infer::{infer_all, infer_positions, InferredPosition};
 use crate::types::{PositionalValue, StrResult};
-use crate::update::update_positions;
+use crate::update::{update_board, update_positions};
 use rand::rngs::ThreadRng;
-use rand::seq::{IteratorRandom};
+use rand::seq::IteratorRandom;
 use std::cmp::min;
-use std::collections::{HashMap};
+use std::collections::HashMap;
 
 pub fn simple_solve(board: &Board) -> StrResult<Board> {
-    let analyzed_board = analyze_board(board)?;
+    let mut analyzed_board = analyze_board(board)?;
     let mut rng_thread = rand::thread_rng();
 
-    return solve_analyzed(&analyzed_board, &mut rng_thread);
+    update_board(&mut analyzed_board)?;
+
+    return solve_analyzed(&analyzed_board, &mut rng_thread, 0);
 }
 
-// will return a cell where:
-// least amount of options
-// including least frequent options
+/**
+@returns return a cell where:
+- Least amount of options.
+- Sort options by least frequent options.
+*/
 fn guess_cell<'a>(
     board: &'a AnalyzedBoard,
     thread_rng: &mut ThreadRng,
-) -> Option<PositionalValue<&'a Vec<usize>>> {
+) -> Option<PositionalValue<Vec<usize>>> {
     let flattened_board = board.get_flat();
 
     let mut flatten_known = Vec::<usize>::new();
-    let mut known_repeat = HashMap::<usize, usize>::new();
+    let mut known_frequency = HashMap::<usize, usize>::new();
     let mut flatten_unknown = Vec::<PositionalValue<&'a Vec<usize>>>::new();
     let mut min_length = board.get_size();
 
@@ -34,9 +38,9 @@ fn guess_cell<'a>(
             AnalyzedCell::Value(v) => {
                 flatten_known.push(*v);
 
-                let curr = known_repeat.get(v).unwrap_or(&0);
+                let curr = *known_frequency.get(v).unwrap_or(&0);
 
-                known_repeat.insert(*v, *curr + 1);
+                known_frequency.insert(*v, curr + 1);
             }
             AnalyzedCell::Undetermined(options) => {
                 flatten_unknown.push(PositionalValue {
@@ -52,35 +56,45 @@ fn guess_cell<'a>(
 
     let chosen = flatten_unknown
         .iter()
-        .filter(|x| x.value.len() == min_length)
+        .filter(|&x| x.value.len() == min_length)
         .choose(thread_rng)?;
 
-    return Some(chosen.clone());
+    let mut options = chosen.value.to_vec();
+
+    let get_frequency = |k:&usize| known_frequency.get(k).unwrap_or(&0);
+
+    options.sort_by(|a, b| get_frequency(a).cmp(get_frequency(b)));
+
+    Some(PositionalValue {
+        row: chosen.row,
+        col: chosen.col,
+        value: options,
+    })
 }
 
-fn solve_analyzed(board: &AnalyzedBoard, rng_thread: &mut ThreadRng) -> StrResult<Board> {
+fn solve_analyzed(board: &AnalyzedBoard, rng_thread: &mut ThreadRng, rec: usize) -> StrResult<Board> {
+    println!("[^] Entering limit {rec}");
+
     let mut analyzed_board = board.clone();
     let mut positions: Vec<InferredPosition> = infer_all(&analyzed_board)?;
     let mut changed_positions: Vec<(usize, usize)> = Vec::new();
 
+    println!("{}", &analyzed_board);
+
     // updating the board until there is nothing to update
     while positions.len() > 0 {
+        changed_positions.clear();
+
         for pos in positions.iter() {
             analyzed_board.set(pos.row, pos.col, AnalyzedCell::Value(pos.value))?;
+            changed_positions.push((pos.row, pos.col));
         }
 
-        let inferred_positions: Vec<(usize, usize)> =
-            positions.iter().map(|x| (x.row, x.col)).collect();
+        let updated_positions = update_positions(&mut analyzed_board, &changed_positions)?;
 
-        let updated_positions = update_positions(&mut analyzed_board, &inferred_positions)?;
-
-        changed_positions.clear();
-        changed_positions.extend(&inferred_positions);
         changed_positions.extend(&updated_positions);
 
         positions = infer_positions(&analyzed_board, &changed_positions)?;
-
-        println!("{}", &analyzed_board);
     }
 
     if is_full_board(&analyzed_board) {
@@ -95,7 +109,10 @@ fn solve_analyzed(board: &AnalyzedBoard, rng_thread: &mut ThreadRng) -> StrResul
             println!("---trying to solve {row},{col} -> {opt}");
             let mut cloned = board.clone();
             cloned.set(row, col, AnalyzedCell::Value(*opt))?;
-            let res = solve_analyzed(&cloned, rng_thread);
+
+            update_positions(&mut cloned, &vec![(row, col)])?;
+
+            let res = solve_analyzed(&cloned, rng_thread, rec + 1);
 
             match res {
                 Ok(res) => {
