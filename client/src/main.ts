7cb744fcb2b0613cd7ad8c9c23e69efe7345b87f
Matanel Mac <mmgordon.matanel@gmail.com>
2023-04-14 14:14:52 +0300

    added vueprime css

--- client/src/main.ts
+++ client/src/main.ts
@@ -1,6 +1,11 @@
 import { createApp } from 'vue';
 import { createPinia } from 'pinia';
+import PrimeVue from 'primevue/config';
+
 import 'normalize.css';
+import 'primevue/resources/themes/md-dark-indigo/theme.css';
+import 'primevue/resources/primevue.min.css';
+import 'primeicons/primeicons.css';
 
 import App from './App.vue';
 import './styles/index.less';
@@ -8,5 +13,6 @@ import './styles/index.less';
 const app = createApp(App);
 
 app.use(createPinia());
+app.use(PrimeVue);
 
 app.mount('#app');
