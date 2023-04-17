34883ebce31aa854ddc08192f004bc6a61405145
Matanel Mac <mmgordon.matanel@gmail.com>
2023-04-17 18:18:16 +0300

    removed wasm-lib submodule

---
+++ wasm-lib/src/lib.rs
@@ -0,0 +1,52 @@
+extern crate core;
+
+mod utils;
+use sudoku_core::board::{Board, BoardData};
+use sudoku_core::solve::simple_solve;
+use wasm_bindgen::prelude::*;
+
+// When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
+// allocator.
+#[cfg(feature = "wee_alloc")]
+#[global_allocator]
+static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
+
+#[wasm_bindgen]
+extern "C" {
+    fn alert(s: &str);
+
+    #[wasm_bindgen(js_namespace = console)]
+    fn log(s: &str);
+}
+
+macro_rules! console_log {
+    // Note that this is using the `log` function imported above during
+    // `bare_bones`
+    ($($t:tt)*) => (log(&format_args!($($t)*).to_string()))
+}
+
+#[wasm_bindgen]
+pub fn greet() {
+    alert("Hello, wasm-lib!");
+}
+
+#[wasm_bindgen]
+pub fn solve(arr: &[usize]) -> Result<Vec<usize>, String> {
+    let flat_data = Vec::from(arr);
+    let square_size = (flat_data.len() as f32).sqrt().floor() as usize;
+    let data = flat_data
+        .chunks(square_size)
+        .map(|slice| slice.to_vec())
+        .collect::<BoardData>();
+
+    let board = Board::from(&data)?;
+    let solved = simple_solve(&board)?;
+
+    let solved_flat = solved
+        .get_rows_flat()
+        .iter()
+        .map(|&x| *x)
+        .collect::<Vec<_>>();
+
+    return Ok(solved_flat);
+}
