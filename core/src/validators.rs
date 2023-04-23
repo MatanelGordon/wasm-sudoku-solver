be61886d2a7001e8850c4bb688f37eedb214d306
Matanel Mac <mmgordon.matanel@gmail.com>
2023-04-23 20:22:21 +0300

    improved validation algorithem

--- core/src/validators.rs
+++ core/src/validators.rs
@@ -16,20 +16,20 @@ pub fn is_square_matrix<T>(data: &BoardData<T>) -> bool {
 
 pub fn is_valid_sudoku(board: &Board) -> bool {
     let size = board.get_size();
+    let square_size = board.get_square_size();
 
-    for row in 0..size {
-        for col in 0..size {
-            let rows = board.get_row(row).unwrap();
-            let cols = board.get_col(col).unwrap();
-            let square = board.get_square_of(row, col).unwrap();
+    for axis in 0..size{
+        let rows = board.get_row(axis).unwrap();
+        let cols = board.get_col(axis).unwrap();
+        let square = board.get_square(axis / square_size, axis % square_size).unwrap();
 
-            for group in vec![rows, cols, square].into_iter() {
-                let is_repeating = group.iter().any(|val| *val > 0 && group.iter().filter(|&x| x == val).count() > 1);
-                if is_repeating {
-                    return false
-                }
+        for group in vec![rows, cols, square].into_iter() {
+            let is_repeating = group.iter().any(|val| *val > 0 && group.iter().filter(|&x| x == val).count() > 1);
+            if is_repeating {
+                return false
             }
         }
     }
+
     true
 }
