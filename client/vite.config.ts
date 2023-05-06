9d318656853c3793251444ccfc10e2f31eb9eee6
Matanel Mac <mmgordon.matanel@gmail.com>
2023-05-06 18:06:13 +0300

    moved dist folder to parent

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -8,6 +8,9 @@ import topLevelAwait from 'vite-plugin-top-level-await';
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [vue(), vueJsx(), wasm(), topLevelAwait()],
+	build: {
+		outDir: '../dist'
+	},
 	resolve: {
 		alias: {
 			'@': fileURLToPath(new URL('./src', import.meta.url))
